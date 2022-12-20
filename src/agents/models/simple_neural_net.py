e87cabc6e76659cb74549ec1535beefef2e61440
Geeeeorge <yuto.george@icloud.com>
2022-12-20 14:32:28 +0900

    fix file name

---
+++ src/agents/models/simple_neural_net.py
@@ -0,0 +1,26 @@
+from keras.layers import Input, concatenate, Dense, Flatten
+from keras.models import Model
+
+
+class SimpleNeuralNet:
+    def __init__(self, input_shape, nb_output):
+        self.input_shape = input_shape
+        self.nb_output = nb_output
+
+    def _build_network(self):
+        input_layer = Input(shape=self.input_shape)
+        x = Flatten()(input_layer)
+        x = Dense(32, activation='relu')(x)
+        x = Dense(32, activation='relu')(x)
+        x = Dense(32, activation='relu')(x)
+        output_layer = Dense(self.nb_output, activation='linear')(x)
+        model = Model(inputs=input_layer, outputs=output_layer)
+        summary = model.summary()
+
+        return model, summary
+
+    def model(self):
+        return self._build_network()[0]
+
+    def summary(self):
+        return self._build_network()[1]
